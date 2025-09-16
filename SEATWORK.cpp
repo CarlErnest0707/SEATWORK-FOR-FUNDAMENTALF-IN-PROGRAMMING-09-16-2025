0e90a1638fa760055fb44abaa3eb330e78362472
CarlErnest0707 <2025-1-01107@lpu.edu.ph>
2025-09-16 08:15:53 +0800

    Add files via upload

---
+++ SEATWORK.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+int main() {
+    int grades; 
+    printf("Please input your grade: ");
+    scanf("%d", &grades);
+    switch (grades) {
+    case 100:
+    case 99:
+    case 98:
+    case 97:
+    case 96:
+    case 95:
+    case 94:
+    case 93:
+    case 92:
+    case 91:
+    case 90:
+    printf("A");
+    break;
+    case 89:
+    case 88:
+    case 87:
+    case 86:
+    case 85:
+    case 84:
+    case 83:
+    case 82:
+    case 81:
+    case 80:
+    printf("B");
+    break;
+    case 79:
+    case 78:
+    case 77:
+    case 76:
+    case 75:
+    case 74:
+    case 73:
+    case 72:
+    case 71:
+    case 70:
+    case 69:
+    case 68:
+    case 67:
+    case 66:
+    case 65:
+    case 64:
+    case 63:
+    case 62:
+    case 61:
+    case 60:
+    printf("C");
+    break;
+    default:
+    printf("Failed");
+    break;
+    return 0;
+    }
+}
